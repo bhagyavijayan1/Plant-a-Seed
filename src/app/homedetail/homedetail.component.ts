fec75a9fc3971ad7040b301bf7a03589479f4132
Bhagya Vijayan <62507893+bhagyavijayan1@users.noreply.github.com>
2022-02-08 14:13:55 +0400

    Add files via upload

---
+++ src/app/homedetail/homedetail.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-homedetail',
+  templateUrl: './homedetail.component.html',
+  styleUrls: ['./homedetail.component.css']
+})
+export class HomedetailComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
